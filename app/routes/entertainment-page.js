98550e3a7a567e67e9afcc1b95cf97371ae3cd54
Austin Blain and Kevin Finley <austin@>
2017-01-10 13:37:21 -0800

    add all pages content

--- app/routes/entertainment-page.js
+++ app/routes/entertainment-page.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return $.get('https://newsapi.org/v1/articles?source=the-lad-bible&sortBy=top&apiKey=bf1f37239598480f9f9a5d12a5d79e79').then (results => {
+      return results;
+    })
+  }
 });
