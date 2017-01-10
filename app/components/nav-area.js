d0dd29c08e995c10d38762d06e43cd93fb8d8418
Austin Blain and Kevin Finley <kevin@>
2017-01-10 11:46:19 -0800

    add carasel

--- app/components/nav-area.js
+++ app/components/nav-area.js
@@ -3,5 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement() {
     Ember.$(".button-collapse").sideNav();
+    Ember.$('.carousel.carousel-slider').carousel({full_width: true})
   }
 });
